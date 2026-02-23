4d129b71fee30e2ee8778d08ed4cc62bd1058432
Jahidbsl <jahidhasanbsl46@gmail.com>
2026-02-23 12:26:23 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,89 @@
+// count
+function updateCounters() {
+
+    const allCards = document.querySelectorAll('#job-cards .card');
+    document.getElementById("total-count").innerText = allCards.length;
+    const interviewCount = document.querySelectorAll('.card[data-status ="interview"]');
+    document.getElementById('interview-count').innerText = interviewCount.length;
+    const rejectedCount = document.querySelectorAll('.card[data-status ="rejected"]');
+    document.getElementById('rejected-count').innerText = rejectedCount.length;
+}
+function toggleStyle(id) {
+    // console.log("toggle clicked")
+    const buttons = document.querySelectorAll('.toggle-btn');
+    for (btn of buttons) {
+        btn.classList.remove("btn-primary");
+    }
+    const selected = document.getElementById(id);
+    if (selected) {
+        selected.classList.add("btn-primary");
+    }
+    // console.log(selected);
+
+    
+    const sectionEmpty = document.querySelector(".section-empty");
+    const allCards = document.querySelectorAll('#job-cards .card');
+    let visibleCount = 0;
+    for (card of allCards) {
+        const status = card.getAttribute('data-status');
+
+        if (id === 'all-job' || status === id) {
+            card.style.display = "block";
+            visibleCount++;
+        }
+        else {
+            card.style.display = "none";
+
+        }
+    }
+    if (visibleCount === 0) {
+        sectionEmpty.classList.remove('hidden');
+
+    } else {
+        sectionEmpty.classList.add("hidden");
+    }
+
+}
+
+document.getElementById('job-cards').addEventListener('click', function (event) {
+    const target = event.target;
+    const card = target.closest('.card');
+
+
+    if (target.classList.contains('fa-trash')) {
+        const card = target.closest('.card');
+        confirm(" Want to delete ?")
+        card.remove();
+        updateCounters();
+    }
+    if (target.classList.contains('apply')) {
+        const isApplied = target.innerText === "Applied";
+
+        target.innerText = isApplied ? "Not Applied" : "Applied";
+        target.classList.toggle("btn-primary", !isApplied);
+    }
+
+    if (target.innerText.toLowerCase() === 'interview' && target.classList.contains('btn-success')) {
+        const applyBtn = card.querySelector('.apply');
+        if (applyBtn && applyBtn.innerText === "Applied") {
+            card.setAttribute('data-status', 'interview');
+            alert("Status updated to Interview!");
+            updateCounters();
+
+        }
+        else {
+            alert("Apply first!")
+        }
+    }
+
+    if (target.innerText.toLowerCase() === 'rejected' && target.classList.contains('btn-error')) {
+        const card = target.closest('.card');
+        card.setAttribute('data-status', 'rejected');
+        alert("Status updated to Rejected");
+        updateCounters();
+    }
+
+
+});
+
+
